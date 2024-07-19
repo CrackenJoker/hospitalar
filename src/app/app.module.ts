df1e290e353f245b9038c26394d06bc9964b68a4
CrackenJoker <joelsoncandomba85@gmail.com>
2024-07-19 11:54:29 -0400

    hospitalar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { HomeComponent } from './components/dash/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
